ba14095f8bf9853b55239014b837ed0f245266b8
LucasCallamullo <lucascallamullo@hotmail.com>
2025-01-30 13:59:35 -0300

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,6 +19,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# this is for deployment
+ALLOWED_HOSTS = ['127.0.0.1', 'https://project-ecommerce-w-payments.up.railway.app', 'project-ecommerce-w-payments.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://project-ecommerce-w-payments.up.railway.app', 'http://localhost', 'project-ecommerce-w-payments.up.railway.app']
+
+
 # =====================================================================================
 #             EVERYTHING RELATED TO ENVIRONMENTAL VARIABLES n DB
 # =====================================================================================
@@ -207,7 +212,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Add for deploy to use "Whitenoise"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# this is for deployment
-ALLOWED_HOSTS = ['127.0.0.1', 'https://project-ecommerce-w-payments.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://project-ecommerce-w-payments.up.railway.app', 'http://localhost']
