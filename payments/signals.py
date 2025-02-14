22c36dddae3731675373a3b877d281cb3e94a076
LucasCallamullo <lucascallamullo@hotmail.com>
2025-02-13 23:54:07 -0300

    add signals send email to order

--- payments/signals.py
+++ payments/signals.py
@@ -9,11 +9,12 @@ def send_invoice_email(sender, instance, created, **kwargs):
     # instance hace referencia al objeto modelo agregado en sender
     
     if created:  # Solo se ejecuta cuando la factura es creada por primera vez
+        factura_number = f"FAC-{instance.id:06d}"
         subject = 'Pago Confirmado - Gracias por tu compra'
         text_content = f'Hola {instance.name}, tu pago por la factura #{instance.invoice_number} ha sido confirmado.'
         html_content = f'''
             <p>Hola <strong>{instance.name}</strong>,</p>
-            <p>Tu pago por la factura <strong>#{instance.invoice_number}</strong> ha sido confirmado.</p>
+            <p>Tu pago por la factura <strong>#{factura_number}</strong> ha sido confirmado.</p>
             <p>Gracias por tu compra.</p>
         '''
         msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [instance.email])
