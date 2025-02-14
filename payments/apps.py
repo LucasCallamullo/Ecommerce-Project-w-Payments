989016b02cc70db93a11f30582a0c39b3d89bb01
LucasCallamullo <lucascallamullo@hotmail.com>
2025-02-13 23:15:45 -0300

    add signals send email to order

--- payments/apps.py
+++ payments/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
+    
+    # Asegura que las señales se carguen
+    def ready(self):
+        import payments.signals  
